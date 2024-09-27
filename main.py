a37ed5795696bc25f13284ae745eec54168943b0
asarmadi <a.sarmadi@yahoo.com>
2024-09-27 13:09:10 -0400

    tqdm added

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ from utils.system import CartPole
 from utils.visualization import CartPoleVisualizer
 
 from scipy.optimize import minimize
+from tqdm import tqdm
 
 
 # Initialize state and storage
-x0          = np.array([0, 0, np.pi+0.1, 0])  # [x, x_dot, theta, theta_dot]
+x0          = np.array([0, 0, 0, 0])  # [x, x_dot, theta, theta_dot]
 xf          = np.array([0, 0, np.pi, 0]) 
-mpc_horizon = 100
+
 
 history = []
 config = Config()
 system = CartPole(config, xf)
-system.init_controller(xf, mpc_horizon)
+system.init_controller(xf, config.mpc_horizon)
 
 # Simulate the dynamics
-for t in np.arange(0, config.T, config.dt):
+for t in tqdm(np.arange(0, config.T, config.dt)):
     action = system.controller(x0)
     x0 = system.step(x0, action)
     history.append(x0)
