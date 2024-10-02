e16baa6b1430044fd8ed71d2f7d418b73c53e4c2
asarmadi <a.sarmadi@yahoo.com>
2024-10-02 19:45:34 -0400

    Inertia and time step updated

--- utils/config.py
+++ utils/config.py
@@ -5,6 +5,7 @@ class Config:
         self.m_cart = 1.0  # mass of the cart, in kg
         self.m_pole = 0.1  # mass of the pole, in kg
         self.l = 1.0  # length of the pole, in meters
-        self.dt = 0.02  # time step, in seconds
+        self.J = (self.m_pole * self.l**2) # Moment of inertia
+        self.dt = 0.04  # time step, in seconds
         self.T = 10  # total time, in seconds
         self.mpc_horizon = 100 # Horizon lenght in time step
